e98e2baf38079d790acaff8dd36ce9217153025f
Jeong-cheol Lee <tree4553@naver.com>
2018-09-17 22:17:10 +0900

    카카오 2017년 1차 1번문제
    카카오 2017년 1차 1번문제
    https://programmers.co.kr/learn/courses/30/lessons/17681

--- Algorithm/src/kakao_2017_1_1.java
+++ Algorithm/src/kakao_2017_1_1.java
@@ -4,18 +4,29 @@ public class kakao_2017_1_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		solution(5,new int[] {9,20,28,18,11},new int[] {30,1,21,17,28});
-		solution(6,new int[] {46,33,33,22,31,50},new int[] {27,56,19,14,14,10});
-		
-	}
-	
-	public static void solution(int n , int[] arr1, int[] arr2) {
-		for(int i = 0 ; i < n ; i++) {
-			String bit_and = Integer.toBinaryString(arr1[i]|arr2[i]);
-			String result = bit_and.replaceAll("1", "#").replaceAll("0", " ");
-			System.out.println(result);
+
+		String[] temp = solution(6, new int[] { 46, 33, 33, 22, 31, 50 }, new int[] { 27, 56, 19, 14, 14, 10 });
+		for (int i = 0; i < 6; i++) {
+			System.out.println(temp[i]);
 		}
-		return ;
+		solution(5, new int[] { 9, 20, 28, 18, 11 }, new int[] { 30, 1, 21, 17, 28 });
+
+		solution(6, new int[] { 46, 33, 33, 22, 31, 50 }, new int[] { 27, 56, 19, 14, 14, 10 });
+
+	}
+
+	public static String[] solution(int n, int[] arr1, int[] arr2) {
+		String[] answer = new String[n];
+		for (int i = 0; i < n; i++) {
+			int and_num = arr1[i] | arr2[i];
+			int target_bit = 1;
+			String result = "";
+			for (int j = 0; j < n; j++) {
+				result = ((and_num & target_bit) > 0 ? "#" : " ") + result;
+				target_bit = target_bit << 1;
+			}
+			answer[i] = result;
+		}
+		return answer;
 	}
 }
