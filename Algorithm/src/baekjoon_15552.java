fd79a3f440f17be16d67af719d8210fe75b96fa5
Jeong-cheol Lee <tree4553@naver.com>
2018-09-27 22:40:31 +0900

    백준 15552번
    백준 15552번
    https://www.acmicpc.net/problem/15552

---
+++ Algorithm/src/baekjoon_15552.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class baekjoon_15552 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int testCase = Integer.parseInt(br.readLine());
+		int a=0, b=0;
+
+		for (int i = 0; i < testCase; i++) {
+			try {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			bw.write(a + b + "\n");
+			}catch(Exception e) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				b = Integer.parseInt(st.nextToken());
+				bw.write(a + b + "\n");
+			}
+		}
+		bw.flush();
+
+	}
+}
