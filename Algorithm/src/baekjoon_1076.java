aee9546a1febf6b7caa29e2abfcb22be51ea92f0
tree4553 <tree4553@gmail.com>
2018-10-10 20:51:09 +0900

    백준 1076번
    백준 1076번
    https://www.acmicpc.net/problem/1076

---
+++ Algorithm/src/baekjoon_1076.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.*;
+
+public class baekjoon_1076 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] table = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+		String temp;
+		long result = 0;
+		temp = br.readLine();
+		for (int i = 0; i < table.length; i++) {
+			if (temp.equals(table[i])) {
+				result += 10 * i;
+				break;
+			}
+		}
+		temp = br.readLine();
+		for (int i = 0; i < table.length; i++) {
+			if (temp.equals(table[i])) {
+				result += i;
+				break;
+			}
+		}
+		temp = br.readLine();
+		for (int i = 0; i < table.length; i++) {
+			if (temp.equals(table[i])) {
+				result *= Math.pow(10, i);
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+}
