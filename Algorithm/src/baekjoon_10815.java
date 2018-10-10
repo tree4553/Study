3f70b9e2a30f0640fb071d5015b5018893804c15
tree4553 <tree4553@gmail.com>
2018-10-10 20:03:54 +0900

    백준 10815
    백준 10815
    https://www.acmicpc.net/problem/10815
    
    bw.write를 사용하려고 배열에다가 저장하고 출력하는게 printf보다 느렸음

--- Algorithm/src/baekjoon_10815.java
+++ Algorithm/src/baekjoon_10815.java
@@ -1,5 +1,10 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class baekjoon_10815 {
 
@@ -7,13 +12,53 @@ public class baekjoon_10815 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		LinkedList<Integer> list1 = new LinkedList<>();
-		int a = Integer.parseInt(br.readLine());
+		int n1 = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < a ; i++ ) {
-			list1.add(Integer.parseInt(st.nextToken()));
+		int[] arr1 = new int[n1];
+		for (int i = 0; i < n1; i++) {
+			arr1[i] = Integer.parseInt(st.nextToken());
 		}
-		
+		Arrays.sort(arr1);
+		int n2 = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		int[] arr2 = new int[n2];
+		for (int i = 0; i < n2; i++) {
+			arr2[i] = Integer.parseInt(st.nextToken());
+		}
+		String answer = "";
+		for(int i = 0 ; i < n2 ; i++){
+//			answer += solution(arr2[i],arr1);
+//			if(i == n2){
+//				answer += "\n";
+//			}
+//			else{
+//				answer += " ";
+//			}
+			System.out.print(solution(arr2[i],arr1) + " ");
+		}
+//		bw.write(answer);
+//		bw.flush();
+//		bw.close();
 	}
-
+	public static int solution(int n , int[] arr){
+		int min = 0;
+		int max = arr.length;
+		int middle = 0;
+		while(max-min>1){
+			middle = (min+max)/2;
+			if(arr[0] == n){
+				return 1;
+			}
+			if(arr[middle] == n){
+				return 1;
+			}
+			if(arr[middle] > n){
+				max = middle;
+			}
+			else if(arr[middle] < n){
+				min = middle;
+			}
+		}
+		return 0;
+	}
 }
