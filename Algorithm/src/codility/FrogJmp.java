d36460c9a2e98d357d03956a557ba83fccc79ffa
Jeong-cheol Lee <tree4553@naver.com>
2018-11-04 00:42:19 +0900

    Update Algorithm/src/codility/FrogJmp.java
    https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/

---
+++ Algorithm/src/codility/FrogJmp.java
@@ -0,0 +1,20 @@
+package codility;
+
+public class FrogJmp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10, y = 85, d = 30;
+		int result = solution(x, y, d);
+		System.out.println(result);
+	}
+
+	public static int solution(int X, int Y, int D) {
+		int result = 0;
+		if ((Y - X) % D > 0) {
+			return (Y - X) / D + 1;
+		} else {
+			return (Y - X) / D;
+		}
+	}
+}
