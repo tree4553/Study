9fd6b27ab9cca30b5d0245c219f17f901fc7964b
Jeong-cheol Lee <tree4553@naver.com>
2018-11-04 04:11:52 +0900

    PassingCars_11_timeout
    PassingCars_11_timeout

---
+++ Algorithm/src/codility/PassingCars_11_timeout.java
@@ -0,0 +1,26 @@
+package codility;
+
+public class PassingCars_11_timeout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] A = { 0, 1, 0, 1, 0 };
+		int result = solution(A);
+		System.out.println(result);
+	}
+
+	public static int solution(int[] A) {
+		int result = 0;
+		int size = A.length;
+		for (int i = 0; i < size; i++) {
+			if (A[i] == 0) {
+				for (int j = i+1; j < size; j++) {
+					if(A[j] == 1) {
+						result++;
+					}
+				}
+			}
+		}
+		return result;
+	}
+}
