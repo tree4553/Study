464d35e37c170a0d50c0100803c8fd6e469b3d99
Jeong-cheol Lee <tree4553@naver.com>
2018-11-22 20:48:56 +0900

    Update Algorithm/src/SW_ExpertAcademy/SW_2971.java

--- Algorithm/src/SW_ExpertAcademy/SW_2971.java
+++ Algorithm/src/SW_ExpertAcademy/SW_2971.java
@@ -7,7 +7,7 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class SWEA_2971{
+public class SW_2971{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
